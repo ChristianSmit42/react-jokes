c178069dd2a75c25ef64f23eb27d8c6273a4adeb
Christian Smit <chrsmit@web.de>
2020-06-19 12:09:16 +0200

    Remove unused imports

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LogoArea from "./Header/LogoArea";
-import {Grid, AppBar, Menu, MenuItem, Button} from "@material-ui/core";
+import {AppBar} from "@material-ui/core";
 import Navigation from "./Header/Navigation";
 
 export default function Header() {
