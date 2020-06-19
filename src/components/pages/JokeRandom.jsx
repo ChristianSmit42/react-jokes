e2dd4620b1bead574f62ad8b616cf7e54806295f
Christian Smit <chrsmit@web.de>
2020-06-19 17:50:24 +0200

    Add pages

---
+++ src/components/pages/JokeRandom.jsx
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from "react";
+import JokeCard from "../Card/JokeCard";
+
+export default function JokeRandom() {
+
+
+    const [joke, setJoke] = useState();
+
+    useEffect(() => {
+            fetch("https://official-joke-api.appspot.com/random_joke").then(response => response.json()).then(data => setJoke(data))
+        }, []
+    );
+    return (
+        <>{joke && <JokeCard joke={joke}></JokeCard>}</>
+    );
+}
