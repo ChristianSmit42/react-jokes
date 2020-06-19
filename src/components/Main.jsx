5fe11bd97457e54c7a60161228bc8fce0a51c392
Christian Smit <chrsmit@web.de>
2020-06-19 15:25:54 +0200

    Add function useEffect

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Main.css";
+import JokeCard from "./Card/JokeCard";
 
-export default function Main(){
-    return(
-        <main className={"Main"}><h1>Main</h1></main>
+export default function Main() {
+
+    const [joke, setJoke] = useState();
+
+    useEffect(() => {
+            fetch("https://official-joke-api.appspot.com/random_joke").then(response => response.json()).then(data => setJoke(data))
+        },[]
+    );
+    return (
+        <main className={"Main"}>
+            <h1>Main</h1>
+            <JokeCard></JokeCard>
+        </main>
     );
 }
